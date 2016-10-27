6e2fb5478420d3d55a32b23ea9bb5555aab2e36d
Kit <bmartin@rocketlawyer.com>
2016-10-26 17:27:00 -0700

    Cleanup

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ var loaderUtils = require('loader-utils');
 
 module.exports = function(content) {
   if (this.cacheable) { this.cacheable(); }
-  
-  var query = loaderUtils.parseQuery(this.query);
-  if (typeof query === "object" && query.regex && typeof query.sub === "string") {
-    var regex = new RegExp(query.regex, query.flags);
-    content = content.replace(regex, query.sub);
+  var reg1 = /<commute(.*?)><\/commute>/g
+
+  function replaceFunc(item) {
+    var key = item.match(/\[key\]\s?=\s?["\']\s?(["\'].*?["\'])/);
+    if(key == null || key[1] == void 0) {
+      // Strip the commute completely because its not correct
+      return '';
+    } else {
+      key = key[1]
+      var params = item.match(/\[params\]\s?=\s?["\']\s?({.*?})["\']/);
+      if(params == null || params[1] == void 0) {
+        return '{{ ' + key + ' | translate }}';
+      } else {
+        return '{{ ' + key + ' | translate:'+params[1]+' }}';
+      }
+    }
   }
+
+  content = content.replace(reg1, replaceFunc);
 
   return content;
 };
